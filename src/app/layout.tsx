34572b74cf8468053859ac082d5c8bbeb88f8c9f
mattduff36 <mattduff36@gmail.com>
2025-05-19 22:50:55 +0100

    feat: add favicon assets and update layout for favicon support; fix: downgrade Next.js to 14.0.3 for stability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_files/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_files/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_files/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon_files/site.webmanifest" />
+      </head>
       <body className={`${inter.className} bg-white`}>
         <StructuredData />
         <Navbar />
